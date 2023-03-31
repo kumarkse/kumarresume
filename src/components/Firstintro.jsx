a2c7c93d861656af23cb99433987352b8d3647ce
kumarkse <abhishektiwari2100@gmail.com>
2023-03-31 16:19:36 +0530

    second page finished

--- src/components/Firstintro.jsx
+++ src/components/Firstintro.jsx
@@ -8,9 +8,10 @@ const Firstintro = () => {
           <p className="n1 bolder">Hi i'm</p>
           <p className="n2 bolder">Kumar Abhishek</p>
           <p className="n3 bolder">I Am A Passionate Developer</p>
-          <p className="n4">I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores.</p>
+          <p className="n4">I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores. Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus corporis molestiae fugiat sunt qui quae dolorum beatae maiores omnis. Enim cupiditate similique repellat quo facere laboriosam dolorum magni deserunt aut?</p>
           <div className="button">
           <button className="cvbtn"><i class="fa-solid fa-download"></i>Download CV</button>
+          <button className="hirebtn cvbtn"><i class="fa-solid fa-download"></i>Hire-me</button>
           </div>
             
           </div>
--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 const Second = () => {
   return (
-    <div className='second allright'>nj </div>
+    <div className='second allright'> 
+    <div className="heading">
+      About Me
+    </div>
+    <hr className="line" />
+    <div className="content">
+      <div className="img">
+      </div>
+
+      <div className="data">
+        <p className="title">
+          Hello,
+        </p>
+        <p className="para">
+        I am Bako Doe, web developer from London, United Kingdom. I have rich experience in web site design and building and customization, also I am good at wordpress. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur quidem, quia a vero illo repellendus magni consequatur nulla suscipit dolorum accusamus! Dolores, non ullam tenetur possimus accusantium eveniet magnam facilis.   
+        </p>
+        <p className="detail">
+          <ul className="lefty">
+            <li className="dark">Name : <span className="light">Kumar Abhishek</span></li>
+            <li className="dark">Age : <span className="light">19</span></li>
+            <li className="dark">D.O.B : <span className="light">11 jan 2004</span></li>
+          </ul>
+          <ul className="lefty ">
+            <li className="dark">Residence : <span className="light">India</span></li>
+            <li className="dark">Address: <span className="light">Ghaziabad</span></li>
+            <li className="dark">E-mail: <span className="light">abhishektiwari2100@gmail.com</span></li>
+          </ul>
+        </p>
+      <div ><button className="bttn"><i class="fa-solid fa-phone"></i>Contact me</button></div>
+      </div>
+      </div>
+    </div>
   )
 }
 
